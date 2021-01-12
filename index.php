1f16bea1c31be8266437bb4540e93f4599987666
rahuldeshmukh997 <75068009+rahuldeshmukh997@users.noreply.github.com>
2021-01-12 11:47:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,26 +2,27 @@
     
     include_once 'header.php'
 
-?>         
-            <?php
+?>   
+      
+        <?php
 
-                if(isset($_SESSION["useruid"])){
+        if(isset($_SESSION["useruid"])){
 
-                    echo "<p>Hello " . $_SESSION["useruid"] . "</p>";
+            echo "<p>Hello " . $_SESSION["useruid"] . "</p>";
                 
-                }
-            ?>    
+        }
+        ?>    
 
-            <div class="row">
-                <div class="news-1">
-                    <rssapp-wall id="VKfbIdtm7evBvq80"></rssapp-wall><script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
-                </div>
-            </div>   
-            <div class="row"> 
-                <div class="news-2">
-                    <rssapp-wall id="jvX3M63UiWHhtZwz"></rssapp-wall><script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
-                </div>
+        <div class="row">
+            <div class="news-1">
+                <rssapp-wall id="VKfbIdtm7evBvq80"></rssapp-wall><script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
             </div>
+        </div>   
+        <div class="row"> 
+            <div class="news-2">
+                <rssapp-wall id="jvX3M63UiWHhtZwz"></rssapp-wall><script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
+            </div>
+        </div>
 
 
 <?php
