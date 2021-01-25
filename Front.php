4886919093705800d12ac296dd678420dc6773d4
rahuldeshmukh997 <75068009+rahuldeshmukh997@users.noreply.github.com>
2021-01-26 00:38:45 +0530

    Front Page

---
+++ Front.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechSpace</title>
+    <link rel="stylesheet" href="css/welcome_style.css">
+    <script>
+        setTimeout(function(){location.href="login.php"} , 5000);  
+    </script>
+</head>
+<body>
+    <a href="/login.html">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <img src="images/crop.jpg" class="img-fluid" alt="logo">
+                </div>
+                <div class="col-6">
+                    <span class="text-1">TechSpace</span>
+                    <span class="text-2">Technology At One Place</span>
+                </div>  
+            </div>
+            
+            <!-- <span class="text-1">TechSpace</span>
+            <span class="text-2">Technology At One Place</span> -->
+        </div>
+    </a>
+</body>
+</html>
