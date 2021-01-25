577ff86462e1b3327b2c91340a13f87023ea7f07
rahuldeshmukh997 <75068009+rahuldeshmukh997@users.noreply.github.com>
2021-01-26 00:19:08 +0530

    Gain Knowledge Section

---
+++ gain.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custome CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/queries_style.css">
+
+    <!-- Responsive CSS File -->
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <!-- Footer CSS -->
+    <link rel="stylesheet" href="css/footer.css"> 
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    
+    <title>TechSpace | Information</title>
+
+    <style type="text/css">
+        body h1{ font: 24px sans-serif; text-align: center; }
+    </style>
+    
+  </head>
+  <body>
+    <!--====================================================== Navigation Bar ========================================================================  -->
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/index.html">
+                <img src="images/crop.jpg" alt="" width="30" height="24" class="d-inline-block align-top">
+                <span>TechSpace</span> 
+            </a>
+            <!-- <a class="navbar-brand" href="#">TechSpace</a> -->
+            <div class="float-end">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ms-5">
+                            <a class="nav-link" aria-current="page" href="index.html">Home</a>
+                        </li>
+                        <li class="nav-item dropdown ms-5">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Knoledge</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Gain Knowledge</a></li>
+                                <li><a class="dropdown-item" href="quiz.index.php">Technical Quiz</a></li>
+                                <!-- <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+                            </ul>
+                        </li>
+                        <li class="nav-item ms-5">
+                            <a class="nav-link" href="index.php">Blog</a>
+                        </li>
+                        <li class="nav-item ms-5">
+                            <a class="nav-link" href="product.index.php">Product Reviews</a>
+                        </li>
+                        <li class="nav-item dropdown ms-5">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Queries</a>
+                            <ul class="dropdown-menu mr-5" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="repair.php">Repair Center</a></li>
+                                <li><a class="dropdown-item" href="seller.php">Authorized seller</a></li>
+                                <!-- <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+                            </ul>
+                        </li>  
+                        <li class="nav-item dropdown ms-5">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profile</a>
+                            <ul class="dropdown-menu mr-5" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="reset-password.php">Reset Password</a></li>
+                                <li><a class="dropdown-item" href="logout.php">Sign Out</a></li>
+                            </ul>
+                        </li>  
+                        <li class="nav-item ms-5">
+                            <a class="nav-link" href="about_us.php">About Us</a>
+                        </li>    
+                        
+                    </ul>
+                </div>
+            </div>    
+        </div>
+    </nav>
+
+    <!--====================================================== End Navigation Bar ========================================================================  -->
+
+    <!--====================================================== News Content ========================================================================  -->
+    <div class="pt-5">
+        <div class="mt-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="news-1">
+                        <rssapp-wall id="CAzQCetane3dp04K"></rssapp-wall><script src="https://widget.rss.app/v1/wall.js" type="text/javascript" async></script>
+                    </div>
+                </div>   
+            </div>
+        </div>
+    </div>
+
+    <!--====================================================== End News Content ========================================================================  -->
+
+    <!--====================================================== Start Footer ========================================================================  -->
+
+	<footer class="footer-distributed">
+
+        <div class="footer-left">
+            <!-- <h3>Tech<span>Space</span></h3>
+
+            <p class="footer-links">
+                <a href="#">Home</a>
+                |
+                <a href="#">Blog</a>
+                |
+                <a href="#">Reviews</a>
+                |
+                <a href="#">About</a>
+            </p>
+
+            <p class="footer-company-name">© 2021 TechSpace<br>Developed by Suyash Deshmukh</p> -->
+        </div> 
+
+        <div class="footer-center">
+            <!-- <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>Dombivli</span>
+                    Mumbai</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+91 9898989898</p>
+            </div>
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:support@TechSpace.com">support@TechSpace.com</a></p>
+            </div> -->
+            <h3>Tech<span>Space</span></h3>
+
+            <!-- <p class="footer-links">
+                <a href="#">Home</a>
+                |
+                <a href="#">Blog</a>
+                |
+                <a href="#">Reviews</a>
+                |
+                <a href="#">About</a>
+            </p><br>   -->
+
+            <br><p class="footer-company-name">© 2021 TechSpace <br> Developed by Suyash Deshmukh & Suyash Deshmukh</p>
+        </div>
+        <!-- <div class="footer-right">
+            <p class="footer-company-about">
+                <span>About TechSpace</span>
+                Techspace as name suggests provide all the technology or technical related information at one place.</p>
+            <div class="footer-icons">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-youtube"></i></a>
+            </div> 
+        </div>  -->
+    </footer>
+
+
+<!--====================================================== End Footer ========================================================================  -->
+
+    <!-- jQuery File -->
+    <script src="js/jquery.min.js"></script>
+    
+    <!-- Bootstrap jQuery file -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
